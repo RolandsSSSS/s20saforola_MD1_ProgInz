e2c145a76ad7e2ea7487d933d9e18fbb7ca4296a
RolandsSSSS <s20saforola@venta.lv>
2023-06-27 17:55:52 +0300

    New: new project for MD1

---
+++ src/main/java/lv/venta/S20saforolaMd1ProgInzApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S20saforolaMd1ProgInzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S20saforolaMd1ProgInzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/venta/S20saforolaMd1ProgInzApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S20saforolaMd1ProgInzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
